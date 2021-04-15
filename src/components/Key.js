b3494086b0c433e4079d7f409bbb56c3a7f9f6f5
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-04-15 00:03:53 -0600

    style

--- src/components/Key.js
+++ src/components/Key.js
@@ -7,8 +7,17 @@ export default class Key extends Component {
     }
 
     render() {
+        const numeric = ['1','2','3','4','5','6','7','8','9','0','.','+/-'];
+        const symbols = ['%','/','x','+','-','ce','c'];
         return (
-            <button className="key" onClick={()=>{this.props.pressedButton(this.props.value)}}>
+            <button 
+                className={
+                    numeric.includes(this.props.value) ? "btn btn-light" :
+                    symbols.includes(this.props.value) ? "btn btn-info" :
+                    "btn btn-success"
+                }
+                onClick={()=>{this.props.pressedButton(this.props.value)}} 
+            >
                 <p>{this.props.value}</p>
             </button>
         )
